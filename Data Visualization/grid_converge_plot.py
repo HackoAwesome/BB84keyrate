8b74cbf4eb80bfb6699f197b63d18848d728b3ed
hayleylim05 <hayleylim05@gmail.com>
2026-04-05 22:27:01 +0800

    Update grid_converge_plot.py

--- Data Visualization/grid_converge_plot.py
+++ Data Visualization/grid_converge_plot.py
@@ -2,20 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Step 1: Load the CSV file
-file = "/Users/junhui/Desktop/SP3172/BB84keyrate/Data/grid_converge_result(6).csv"
+file = "/Users/hayleylim/Documents/GitHub/BB84keyrate/Data/grid_converge_result(8).csv"
 data = pd.read_csv(file)
 
-n = "10^6"
+n = "$10^8$"
 
 # Step 2: Plot the data
 plt.plot(data["size of grid"], data["rate"], marker='o', color='b')
 
 # Step 3: Add labels and title
 plt.legend()
-plt.xlabel('Size of Grid(nxn)')
-plt.ylabel('Key Rate')
+plt.xlabel(r'Size of Grid(n $\times$ n)')
+plt.ylabel('Keyrate')
 plt.grid(True)
-plt.title(f"Key Rate for {n} rounds")
+plt.title(f"Keyrate for {n} rounds")
 
 # Step 4: Show the graph
+plt.savefig("/Users/hayleylim/Desktop/SP3172/gridconverge.svg")
 plt.show()
